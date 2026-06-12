48703afaf1206247490720e6e0957f76f49c0af2
snow <zachlathrop84@gmail.com>
2026-06-12 15:26:17 -0400

    upd

--- guis/new.lua
+++ guis/new.lua
@@ -6324,32 +6324,8 @@ general:CreateButton({
 general:CreateButton({
 	Name = "Reinject",
 	Function = function()
-		shared.vapereload = true
-		do
-			local function downloadFile(path, func)
-				if not isfile(path) then
-					local suc, res = pcall(function()
-						return game:HttpGet(
-							"https://raw.githubusercontent.com/snovvvvy/VapeV4ForRoblox/"
-								.. readfile("newvape/profiles/commit.txt")
-								.. "/"
-								.. select(1, path:gsub("newvape/", "")),
-							true
-						)
-					end)
-					if not suc or res == "404: Not Found" then
-						error(res)
-					end
-					if path:find(".lua") then
-						res = "--This watermark is used to delete the file if its cached, remove it to make the file persist after vape updates.\n"
-							.. res
-					end
-					writefile(path, res)
-				end
-				return (func or readfile)(path)
-			end
-
-			loadstring(downloadFile("newvape/main.lua"), "main")()
+			shared.vapereload = true
+			loadstring(game:HttpGet("https://raw.githubusercontent.com/snovvvvy/VapeV4ForRoblox/main/NewMainScript.lua", true))()
 		end
 	end,
 	Tooltip = "Reloads vape for debugging purposes",
