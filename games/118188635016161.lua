a5bd6254f9d97ab922e7705dc6c9a99ad5fab101
snow <zachlathrop84@gmail.com>
2026-07-01 13:33:50 -0400

    upd

--- games/118188635016161.lua
+++ games/118188635016161.lua
@@ -410,7 +410,7 @@ run(function()
 		},
 
 		Piano = {
-			Expand = Vector3.new(6.5, 1000, 6.5),
+			Expand = Vector3.new(10.5, 1000, 10.5),
 
 			Modify = function(obj, size, cframe)
 				local mesh = obj:FindFirstChildWhichIsA("SpecialMesh")
